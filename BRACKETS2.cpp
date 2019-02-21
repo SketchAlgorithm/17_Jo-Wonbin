32fe44a316a57227003d30f3d688354703423b48
SketchAlgorithm <47266632+SketchAlgorithm@users.noreply.github.com>
2019-02-21 16:53:35 +0900

    Update BRACKETS2.cpp

--- BRACKETS2.cpp
+++ BRACKETS2.cpp
@@ -6,18 +6,18 @@
 using namespace std;
 
 int c;
-vector<char> v;
+vector<char> v; //LIFO(last in, first out) 구조 이므로 벡터를 사용
 string br;
 
 bool isMatch() {
 
 	for (int i = 0; i < br.size(); i++) {
 		if (br[i] == '(' || br[i] == '{' || br[i] == '[')
-			v.push_back(br[i]);
+			v.push_back(br[i]); 	//open brackets일 경우 벡터에 저장
 		else if (!v.empty() && (v.back() - br[i] == -1 || v.back() - br[i] == -2))
-			v.pop_back();
-		else
-			return false;
+			v.pop_back();		//close일 경우 맨뒤 값과 ASCII 값이 차이가 1 ,2 일 경우 값을 지우고 for문 진행
+		else	
+			return false;		//mismatch일 경우 return false;
 	}
 
 	return true;
