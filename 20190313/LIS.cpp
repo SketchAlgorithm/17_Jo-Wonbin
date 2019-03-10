aefb1195a6a1fe62c21f573e560aec4d24862711
SketchAlgorithm <47266632+SketchAlgorithm@users.noreply.github.com>
2019-03-10 23:01:08 +0900

    Add files via upload

---
+++ 20190313/LIS.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int N;
+
+vector<int> A, s;
+vector<vector<int> > ret;
+
+int getIdx(int val) {
+	return lower_bound(s.begin(), s.end(), val) - s.begin();
+}
+
+void solve() {
+	int idx;
+	for (int i = 0; i < A.size(); i++) {
+		idx = getIdx(A[i]);
+		if (idx == s.size()) 
+			s.push_back(A[i]);	
+
+		else 
+			s[idx] = A[i]; 	
+			
+		ret[idx].push_back(A[i]);
+	}
+	
+}
+int main() {
+	cin >> N;
+	A.assign(N + 1,-1);
+
+	ret.resize(N);
+	
+	for (int i = 0; i < N; i++)
+		cin >> A[i];
+	solve();
+	int idx;
+	cout << s.size() << endl;
+	A[s.size()] = 1000000001;
+	for (int i = s.size() - 1; i >= 0; i--) {	
+		idx = ret[i].size() - 1;
+		A[i] = ret[i][idx];
+	}
+	for (int i = 0; i < s.size(); i++)
+		cout << A[i] << " ";
+	
+}
