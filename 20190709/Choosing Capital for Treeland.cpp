179626188e2628b4bb71117aaa4f44676b39068e
SketchAlgorithm <47266632+SketchAlgorithm@users.noreply.github.com>
2019-07-04 16:03:47 +0900

    Update Choosing Capital for Treeland.cpp

--- 20190709/Choosing Capital for Treeland.cpp
+++ 20190709/Choosing Capital for Treeland.cpp
@@ -5,18 +5,16 @@
 using namespace std;
 
 int cache[200001];
-vector<int> node[200001];
-vector<int> child[200001];
-vector<int> ans;
-int n, s, t;
+vector<int> parent[200001], child[200001], ans;//adjacency list, ans
+int n, s, t;//input variable
 
-int search(int s, int t) {
+int search(int s, int t) {//brute force
 	
 	int ret = 0;
 
-	for (int i = 0; i < node[t].size(); i++) {
-		if (node[t][i] != s) {
-			ret += 1 + search(t, node[t][i]);
+	for (int i = 0; i < parent[t].size(); i++) {
+		if (parent[t][i] != s) {
+			ret += 1 + search(t, parent[t][i]);
 		}
 	}
 	for (int i = 0; i < child[t].size(); i++) {
@@ -24,17 +22,17 @@ int search(int s, int t) {
 			ret += search(t, child[t][i]);
 		}
 	}
-	
+
 	return ret;
 	
 }
 
-void dp(int s, int t)
+void dp(int s, int t)//memoization
 {
-	for (int i = 0; i < node[t].size(); i++) {
-		if (node[t][i] != s) {
-			cache[node[t][i]] = cache[t] - 1;
-			dp(t, node[t][i]);
+	for (int i = 0; i < parent[t].size(); i++) {
+		if (parent[t][i] != s) {
+			cache[parent[t][i]] = cache[t] - 1;
+			dp(t, parent[t][i]);
 		}
 	}
 	for (int i = 0; i < child[t].size(); i++) {
@@ -46,28 +44,32 @@ void dp(int s, int t)
 }
 
 int main() {
+	//input
 	cin >> n;
 	for (int i = 0; i < n - 1; i++) {
 		scanf("%d %d", &s, &t);
 		child[s].push_back(t);
-		node[t].push_back(s);
+		parent[t].push_back(s);
 	}
+	
 	cache[1] = search(0, 1);
 	dp(0, 1);
+
 	int m = 1000000;
 	for (int i = 1; i < n + 1; i++) {
 		if (cache[i] < m) {
 			ans.clear();
-			ans.push_back(i);
 			m = cache[i];
 		}
-		else if (cache[i] == m)
+		if (cache[i] == m)
 			ans.push_back(i);
 	}
+	//output
 	cout << m << endl;
 	for (int i = 0; i < ans.size(); i++) {
 		printf("%d ", ans[i]);
 	}
+
 	return 0;
 
 }
