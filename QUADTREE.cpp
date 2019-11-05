bd1bd73b2acbd4a579f0b26c79ceec278b3950a4
SketchAlgorithm <47266632+SketchAlgorithm@users.noreply.github.com>
2019-11-05 18:54:53 +0900

    Add files via upload

---
+++ QUADTREE.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int C;
+char input;
+
+string dq() {
+	char t;
+	string tree[4];
+	
+	for (int i = 0; i < 4; i++) {
+		cin >> t;
+		if (t == 'x') 
+			tree[i] = t + dq();
+		else 
+			tree[i] = t;
+	}
+	return tree[2] + tree[3] + tree[0] + tree[1];
+}
+
+int main() {
+
+	cin >> C;
+	while (C-- > 0) {
+		cin >> input;
+		cout << input << (input == 'x' ? dq() : "") << endl;
+	}
+}
